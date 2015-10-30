f630de877d92290c1a05ebc57d60128db8e6db63
AriellaFeuchtwanger <acfeuchtwanger@gmail.com>
2015-10-30 15:20:32 -0400

    Added the mouse Smiley

--- src/feuchtwanger/gui/SmileComponent.java
+++ src/feuchtwanger/gui/SmileComponent.java
@@ -5,29 +5,88 @@ import java.awt.Graphics;
 
 import javax.swing.JComponent;
 
-public class SmileComponent extends JComponent{
+public class SmileComponent extends JComponent {
+	private int y = 200;
+	private int height = 75;
+	private int pupilHeight = 45;
+	private boolean closing = true;
 	
 	@Override
-	protected void paintComponent(Graphics g){
+	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
-		//g.drawLine(0, 300, 600, 50);
+
+		// g.drawLine(0, 300, 600, 50);
 		g.setColor(Color.YELLOW);
-		g.drawOval(230, 130, 300, 300);
+		g.drawOval(230, 130, 300, 300); // can also do getWidth() or getHeight()
 		g.fillOval(230, 130, 300, 300);
+
 		g.setColor(Color.BLUE);
-		g.drawOval(280, 200, 75, 75);
-		g.fillOval(280, 200, 75, 75);
+		g.drawOval(280, y, 75, height);
+		g.fillOval(280, y, 75, height);
+
 		g.setColor(Color.BLACK);
-		g.fillOval(300, 200, 45, 45);
+		g.fillOval(300, y, 45, pupilHeight);
+
 		g.setColor(Color.BLUE);
-		g.drawOval(400, 200, 75, 75);
-		g.fillOval(400, 200, 75, 75);
+		g.drawOval(400, y, 75, height);
+		g.fillOval(400, y, 75, height);
+
 		g.setColor(Color.BLACK);
-		g.fillOval(420, 200, 45, 45);
-		
+		g.fillOval(420, y, 45, pupilHeight);
+
 		g.drawArc(310, 300, 150, 100, 900, 180);
+
 		
+		if (closing) {
+			if (pupilHeight != 0) {
+				pupilHeight--;
+			}
+
+			if (pupilHeight == 0 && height == 0) {
+				pupilHeight++;
+			}
+			if (y == 275) {
+				y--;
+				closing = false;
+			} else {
+				y++;
+			}
+
+			if (height == 0) {
+				height++;
+			} else {
+				height--;
+			}
+		} else {
+			if (pupilHeight != 45) {
+				pupilHeight++;
+			}
+
+			if (pupilHeight == 45 && height == 75) {
+				pupilHeight--;
+			}
+			if (y == 200) {
+				y++;
+				closing = true;
+			} else{
+				y--;
+			}
+			
+			if(height == 75){
+				height--;
+			} else{
+				height++;
+			}
+		}
+
+		try {
+			Thread.sleep(16);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		super.repaint(); // will refresh the screen without having to change the
+							// size, but
+		// then it just keeps going
 	}
 
 }
