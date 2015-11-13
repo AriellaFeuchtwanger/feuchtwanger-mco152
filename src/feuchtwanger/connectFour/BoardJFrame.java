36f67c2e8b697dffcff0576fce6d4abd49572860
unknown <student1@FB-W213-W8.academic.touro.edu>
2015-11-12 20:48:08 -0500

    Played with ConnectFour

--- src/feuchtwanger/connectFour/BoardJFrame.java
+++ src/feuchtwanger/connectFour/BoardJFrame.java
@@ -2,10 +2,16 @@ package feuchtwanger.connectFour;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.GridLayout;
 
 import javax.swing.JFrame;
 
 public class BoardJFrame extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private BoardComponent[][] board;
 	public BoardJFrame() {
 		Game g = new Game();
 		
@@ -13,9 +19,9 @@ public class BoardJFrame extends JFrame {
 		setSize(800, 600);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		BorderLayout layout = new BorderLayout();
+		GridLayout layout = new GridLayout();
 		Container container = getContentPane();
 		container.setLayout(layout);
-		container.add(new BoardComponent(g.getBoard()), BorderLayout.CENTER);
+		
 	}
 }
